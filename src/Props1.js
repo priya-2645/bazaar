534d52aebc5f7ff2ad8095c40b6b9afb0cd6671c
priya <priyamuruganpriya12@gmail.com>
2021-04-23 17:41:50 +0530

    hgg

---
+++ src/Props1.js
@@ -0,0 +1,74 @@
+// import React from 'react';
+// const But= () => {
+//     alert("sucess")
+//     return(
+//         <p>hi</p>
+//     )
+// }
+// export default But;
+
+
+// import React from 'react';
+
+
+// const But =()=>{
+//     alert("ee")
+//     return(
+//         <p>dwsed</p>
+//     )
+// }
+// export default But;
+
+// import React from 'react';
+// import {BrowserRouter as Router,Route,Redirect,Switch} from 'react-router-dom';
+// import Big from "./Big.js"
+// function Routes(){
+//     return (
+//     <Router>
+//       <div>
+//         <Switch>
+//            <Route path="/" component = {Big}/>
+//            <Redirect from='/Big/' to="/But/" />
+//            {/* <Route path="/tutorials/" component={About} /> */}
+//         </Switch>
+//       </div>
+//     </Router>
+//     )
+// }
+// export default Routes;
+// import React from 'react';
+// import { useHistory } from "react-router-dom";
+// ​const Props1 = () => {
+    // let history = useHistory();
+    // const clickfn = () => {
+    //     alert("d")
+    //     history.push('/props')
+    // }
+//     return (
+        // <>
+        //     <p>hiui</p>
+        //     <button onClick={clickfn}> Submit</button>
+        // </>
+// ​
+//     )
+// }
+// export default Props1;
+
+// import React from 'react';
+// import { useHistory } from "react-router-dom";
+
+// const Props1=(props)=>{
+   
+//     let history = useHistory();
+//     const clickfn = () => {
+//         alert("d")
+//         history.push('/props')
+//     }
+//     return(
+//         <>
+//         <p>hiui</p>
+//         <button onClick={clickfn}> Submit</button>
+//     </>
+//     )
+// }
+// export default Props1;
---
+++ src/Props2.js
@@ -0,0 +1,42 @@
+// import React from 'react';
+// ​
+// const Props2 = () => {
+//     alert("/fg")
+//     return (
+// ​
+//         <p>ewdwdw</p>
+//     )
+// }
+// export default Props2;
+
+import Item from 'antd/lib/list/Item';
+import React from 'react';
+const Props2=(props)=>{
+    // console.log(props,"prop")
+    return(
+        <div>
+            <p>Second</p>
+            
+            
+          </div>
+           
+       )
+}
+export default Props2;
+
+
+// import React from 'react';
+// import BigBazaar from './image/Bigbazaar.png'
+// const Props2=()=>{
+//     return(
+//        <div>
+//         <img src={BigBazaar} className="bigbazar" height="46px" alt="Bigbazar Logo" />
+//         <b>BigBazaar</b>
+//         <div className="SuperMarket">
+
+//                       <span title="Super Market">Super Market</span>
+//                     </div>
+//         </div>
+//     )
+// }
+// export default Props2;
